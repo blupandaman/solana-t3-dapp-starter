3bb905fa6798c056bdf60e5c40f34028ebb06930
blupandaman <blupandaman@pm.me>
2022-10-30 17:19:57 -0500

    Initial commit. t3 stack set up with Solana hooks

---
+++ src/pages/_app.tsx
@@ -0,0 +1,36 @@
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { type AppType } from "next/dist/shared/lib/utils";
+import React, { useMemo } from "react";
+
+// Use require instead of import since order matters
+require("@solana/wallet-adapter-react-ui/styles.css");
+require("../styles/globals.css");
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter()],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default MyApp;
